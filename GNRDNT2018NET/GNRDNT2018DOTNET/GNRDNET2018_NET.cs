d8ef754b507238c5401e2cee875d104e2f058bce
tfwittwer <55980052+tfwittwer@users.noreply.github.com>
2019-10-01 13:15:30 +0200

    Add files via upload

---
+++ GNRDNT2018NET/GNRDNT2018DOTNET/GNRDNET2018_NET.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GNRDNT2018DOTNET
+{
+    public interface IGNRDNT2018
+    {
+        int Etrs2rdnap_net(double l, double p, double h, out double x, out double y, out double z);
+        int Rdnap2etrs_net(double x, double y, double z, out double l, out double p, out double h);
+    }
+
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("GNRDNT2018DOTNET.GNRDNT018_NET")]
+    public class GNRDNT2018_NET : IGNRDNT2018 
+    {
+        [DllImport("GNRDNT2018.dll", EntryPoint = "etrs2rdnap")]
+        public static extern int Etrs2rdnap(double l, double p, double h, out double x, out double y, out double z);
+        [DllImport("GNRDNT2018.dll", EntryPoint = "rdnap2etrs")]
+        public static extern int Rdnap2etrs(double x, double y, double z, out double l, out double p, out double h);
+
+        [ComVisible(true)]
+        public int Etrs2rdnap_net(double l, double p, double h, out double x, out double y, out double z)
+        {
+            return Etrs2rdnap(l, p, h, out x, out y, out z);
+        }
+
+        [ComVisible(true)]
+        public int Rdnap2etrs_net(double x, double y, double z, out double l, out double p, out double h)
+        {
+            return Rdnap2etrs(x, y, z, out l, out p, out h);
+        }
+
+    }
+}
---
+++ GNRDNT2018NET/GNRDNT2018NET/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace GNRDNT2018NETTest
+{
+    class Program
+    {
+        [DllImport("GNRDNT2018.dll", EntryPoint = "etrs2rdnap")]
+        public static extern int Etrs2rdnap(double l, double p, double h, out double x, out double y, out double z);
+        [DllImport("GNRDNT2018.dll", EntryPoint = "rdnap2etrs")]
+        public static extern int Rdnap2etrs(double x, double y, double z, out double l, out double p, out double h);
+
+        static void Main(string[] args)
+        {
+            double l = 4.7121201260;
+            double p = 51.7286012740;
+            double h = 301.7981;
+            double x, y, z;
+            Console.WriteLine("input: "+l.ToString("F10")+ " "+ p.ToString("F10") + " "+ h.ToString("F4"));
+            Etrs2rdnap(l, p, h, out x, out y, out z);
+            Console.WriteLine("RD/NAP: " + x.ToString("F4") + " "+y.ToString("F4") + " "+z.ToString("F4"));
+            Rdnap2etrs(x, y, z, out l, out p, out h);
+            Console.WriteLine("check: " + l.ToString("F10") + " " + p.ToString("F10") + " " + h.ToString("F4"));
+
+        }
+    }
+}
